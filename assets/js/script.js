962b0ff612de18ca2d98d1c0c9279cd2fc648086
Ian Tauro <notvalproate>
2023-05-13 15:13:42 +0530

    Added activity info card, js, and video to vision

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Code for setting the brightness of VISION SECTION dynamically
+
+var viewportHeight = window.innerHeight;
+var viewportWidth = window.innerHeight;
+var visionSection = $(".vision-section");
+
+function getScrollPosition() {
+    visionSection.attr("style", `filter: brightness( ${ 100 - ((window.scrollY * 100) / viewportHeight)}% )`);
+}
+
+// Update the two variables in resize of window
+$(window).resize(() => {
+    viewportHeight = window.innerHeight;
+    viewportWidth = window.innerWidth;
+})
+
+// Code for ACTIVITY CARDS
+
+var activityCards = $(".activity-card");
+var activityInfoCards = $(".activity-info-container");
+var activityInfoCard = $(".activity-info-card");
+
+activityCards.click(() => {
+    activityInfoCards.css("visibility", "visible");
+    activityInfoCard.addClass("info-card-in");
+})
+
+activityInfoCards.click(() => {
+    activityInfoCards.css("visibility", "hidden");
+    activityInfoCard.removeClass("info-card-in");
+})
