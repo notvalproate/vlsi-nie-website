a1725165f09699ad234408f27cbe0643dcc30be1
Ian Tauro <notvalproate>
2023-05-14 16:35:31 +0530

    Added json reading and dynamic activity cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,16 +16,60 @@ $(window).resize(() => {
 
 // Code for ACTIVITY CARDS
 
-var activityCards = $(".activity-card");
-var activityInfoContainer = $(".activity-info-container");
-var activityInfoCard = $(".activity-info-card");
+// Code for dynamically adding the cards
 
-activityCards.click(() => {
-    activityInfoContainer.css("visibility", "visible");
-    activityInfoCard.addClass("info-card-in");
-})
+var activityList = $(".activity-list");
 
-activityInfoContainer.click(() => {
-    activityInfoContainer.css("visibility", "hidden");
-    activityInfoCard.removeClass("info-card-in");
-})
+fetch('assets/json/activities.json')
+  .then(response => response.json())
+  .then(data => {
+    const activities = data.activities; 
+    
+    activities.forEach(element => {
+        activityList.append(`
+        <a class="activity-card">
+            <img src="${element.poster_url}" class="activity-poster">
+            <span class="activity-info-hover">Click for more info</span>
+            <h2 class="activity-title">${element.title}</h2>
+            <span class="activity-subtitle">${element.subtitle}</span>
+        </a>
+      `)
+    });
+
+    var activityCards = $(".activity-card");
+    var activityInfoContainer = $(".activity-info-container");
+    var activityInfoCard = $(".activity-info-card");
+
+    var title = $("#title");
+    var date_time = $("#date-time");
+    var poster = $("#poster");
+    var venue = $("#venue");
+    var fee = $("#fee");
+    var description = $("#description");
+    var qr_code = $("#qr-code");
+    var form_link = $("#form_link")
+
+    activityCards.each(function(index) {
+        $(this).on('click', function() {
+            poster.attr("src", `${activities[index].poster_url}`);
+            title.html(`${activities[index].title}`);
+            date_time.html(`${activities[index].date_time}`);
+            venue.html(`${activities[index].venue}`);
+            fee.html(`${activities[index].fee}`);
+            description.html(`${activities[index].description}`);
+            qr_code.attr("src", `${activities[index].qr_code_url}`);
+            form_link.attr("href", `${activities[index].form_link}`);
+
+            activityInfoContainer.css("visibility", "visible");
+            activityInfoCard.addClass("info-card-in");
+        });
+    });
+
+    activityInfoContainer.click(() => {
+        activityInfoContainer.css("visibility", "hidden");
+        activityInfoCard.removeClass("info-card-in");
+    })
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
