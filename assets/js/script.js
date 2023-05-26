d8111bb04eb4c23b676adadebd65fe1444971fa9
Ian Tauro <sushruth127@gmail.com>
2023-05-27 02:42:34 +0530

    Added special case for no activities

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,19 @@ $(window).resize(() => {
 
 // Code for dynamically adding the cards
 
+// TEMPLATE FOR ADDING OBJECTS IN THE JSON
+// {
+//   "title":"AMD Processors",
+//   "subtitle":"Come here to check out how AMD makes it's processors",
+//   "poster_url":"./assets/images/activity-placeholder.png",
+//   "date_time":"6:00PM, May 16",
+//   "venue":"GJB, NIdE",
+//   "fee":"Free for All",
+//   "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tenetur officia voluptates unde ab magnam sit possimus. Ut, sequi, magni cum veniam sed totam es.",
+//   "qr_code_url":"./assets/images/qr-code.png",
+//   "form_link":"https://www.amd.com"
+// },
+
 var activityList = $(".activity-list");
 
 fetch('assets/json/activities.json')
@@ -25,6 +38,15 @@ fetch('assets/json/activities.json')
   .then(data => {
     const activities = data.activities; 
     
+    if(activities.length === 0) {
+      activityList.append(`
+        <span class="no-activity">No Upcoming Activities. Will be updated shortly.</span>
+      `);
+      return;
+    }
+
+    console.log("hi");
+
     activities.forEach(element => {
         activityList.append(`
         <a class="activity-card">
